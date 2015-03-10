1db1c8e77baeb91512ef77ee0fb8bd9ea9b56649
froatsnook <froatsnook@gmail.com>
2015-03-10 22:37:22 +0100

    Update dist for 1.1.0.

--- dist/bardcode.js
+++ dist/bardcode.js
@@ -661,118 +661,7 @@ bardcode.encodeITF = function(text) {
     };
 };
 
-var optionDefaults = {
-    type: "Code 128",
-    x: 0,
-    y: 0,
-    moduleWidth: 2.892,
-    height: 90.72,
-    horizontalAlign: "left",
-    verticalAlign: "top",
-    quietZoneSize: 10,
-    angle: 0,
-    maxWidth: Infinity,
-    width: NaN
-};
-
-// Copy default values from source to target if they don't exist.
-var copyDefaults = function(target, source) {
-    for (var key in source) {
-        if (typeof target[key] === "undefined") {
-            target[key] = source[key];
-        }
-    }
-};
-
-/**
- * @summary Draw a barcode to a canvas graphics context.
- * @todo IMB, Pharmacode, PostBar, POSTNET, Telepen
- * @param {Context2D} g An HTML5 or node-canvas graphics context.
- * @param {String} text Barcode text (without start, end, or check characters).
- * @param {Object} options Controls what barcode is drawn, where, and how.
- * @param {String} options.type Barcode type.  Defaults to Code 128.  Other valid options are "Codabar", "Code 39", "EAN-8", "EAN-13", "FIM", "ITF" (interleaved 2 of 5), and "UPC-A".
- * @param {Number} options.x Where to draw barcode.  Defaults to 0.
- * @param {Number} options.y Where to draw the barcode.  Defaults to 0.
- * @param {String} options.horizontalAlign How to align the barcode.  Defaults to "left".  Other options are "center" and "right".
- * @param {String} options.verticalAlign How to align the barcode.  Defaults to "top".  Other options are "middle" and "bottom".
- * @param {Number} options.height Barcode height.  Defaults to 90.72.
- * @param {Number} options.moduleWidth Width of thinnest bar.  Defaults to 2.892.
- * @param {Number} options.quietZoneSize Number of moduleWidths in quiet zone on either side.  Defaults to 10.
- * @param {Number} options.angle Rotate barcode this many degrees clockwise.  Defaults to 0.
- * @param {Number} options.maxWidth Maximum barcode width (including quiet zones).  If specified, then the moduleWidth will be adjusted if necessary to make the entire barcode fit in the given width.
- * @param {Number} options.width If given, then ignore moduleWidth and maxWidth and set the moduleWidth so that the barcode will have the given width.
- */
-drawBarcode = bardcode.drawBarcode = function(g, text, options) {
-    // Validate input.
-    if (typeof g !== "object") {
-        throw new Error("drawBarcode: expected `g' to be an object.");
-    }
-
-    if (!text || typeof text !== "string") {
-        throw new Error("drawBarcode: expected `text' to be a non-empty string.");
-    }
-
-    if (typeof options !== "object") {
-        options = { };
-    }
-
-    copyDefaults(options, optionDefaults);
-    bardcode.validateDrawBarcodeOptions(options);
-
-    var encodeData;
-    switch (options.type) {
-        case "Codabar":
-            encodeData = bardcode.encodeCodabar(text);
-            break;
-        case "Code 128":
-            encodeData = bardcode.encodeCode128(text);
-            break;
-        case "Code 39":
-            encodeData = bardcode.encodeCode39(text);
-            break;
-        case "ITF":
-            encodeData = bardcode.encodeITF(text);
-            break;
-        case "EAN-8":
-            encodeData = bardcode.encodeEAN(text);
-            break;
-        case "EAN-13":
-            encodeData = bardcode.encodeEAN(text);
-            break;
-        case "FIM":
-            encodeData = bardcode.encodeFIM(text);
-            break;
-        case "UPC-A":
-            encodeData = bardcode.encodeEAN(text);
-            break;
-    }
-
-    switch (encodeData.type) {
-        case "bits":
-            return bardcode.drawBitsBarcode(g, options, encodeData);
-        default:
-            throw new Error("Unrecognized encoded barcode type: " + encodeData.type);
-    }
-};
-
-bardcode.validateDrawBarcodeOptions = function validateDrawBarcodeOptions(options) {
-    bardcode.AssertIsNumber(options.x, "options.x");
-    bardcode.AssertIsNumber(options.y, "options.y");
-    bardcode.AssertIsValidHorizontalAlign(options.horizontalAlign);
-    bardcode.AssertIsValidVerticalAlign(options.verticalAlign);
-    bardcode.AssertIsPositiveNumber(options.height, "options.height");
-    bardcode.AssertIsPositiveNumber(options.moduleWidth, "options.moduleWidth");
-    bardcode.AssertIsNonNegativeNumber(options.quietZoneSize, "options.quietZoneSize");
-    bardcode.AssertIsNumber(options.angle, "options.angle");
-    bardcode.AssertIsPositiveNumber(options.maxWidth, "options.maxWidth");
-
-    // width can either be NaN or a positive number.
-    if (!isNaN(options.width)) {
-        bardcode.AssertIsPositiveNumber(options.width, "options.width");
-    }
-};
-
-bardcode.drawBitsBarcode = function(g, options, encodeData) {
+bardcode.drawBitsBarcodeToCanvas = function(g, options, encodeData) {
     var bits = encodeData.data.map(function(d) {
         return d.bits;
     }).join("");
@@ -923,6 +812,204 @@ bardcode.drawBitsBarcode = function(g, options, encodeData) {
         }
     };
 };
+
+var optionDefaults = {
+    type: "Code 128",
+    x: 0,
+    y: 0,
+    moduleWidth: 2.892,
+    height: 90.72,
+    horizontalAlign: "left",
+    verticalAlign: "top",
+    quietZoneSize: 10,
+    angle: 0,
+    maxWidth: Infinity,
+    width: NaN
+};
+
+// Copy default values from source to target if they don't exist.
+var copyDefaults = function(target, source) {
+    for (var key in source) {
+        if (typeof target[key] === "undefined") {
+            target[key] = source[key];
+        }
+    }
+};
+
+/**
+ * @summary Draw a barcode to a canvas graphics context.
+ * @todo IMB, Pharmacode, PostBar, POSTNET, Telepen
+ * @param {Context2D|String} g An HTML5 or node-canvas graphics context or the output format.  The only supported non-canvas output format is "svg".
+ * @param {String} text Barcode text (without start, end, or check characters).
+ * @param {Object} options Controls what barcode is drawn, where, and how.
+ * @param {String} options.type Barcode type.  Defaults to Code 128.  Other valid options are "Codabar", "Code 39", "EAN-8", "EAN-13", "FIM", "ITF" (interleaved 2 of 5), and "UPC-A".
+ * @param {Number} options.x Where to draw barcode.  Defaults to 0.
+ * @param {Number} options.y Where to draw the barcode.  Defaults to 0.
+ * @param {String} options.horizontalAlign How to align the barcode.  Defaults to "left".  Other options are "center" and "right".
+ * @param {String} options.verticalAlign How to align the barcode.  Defaults to "top".  Other options are "middle" and "bottom".
+ * @param {Number} options.height Barcode height.  Defaults to 90.72.
+ * @param {Number} options.moduleWidth Width of thinnest bar.  Defaults to 2.892.
+ * @param {Number} options.quietZoneSize Number of moduleWidths in quiet zone on either side.  Defaults to 10.
+ * @param {Number} options.angle Rotate barcode this many degrees clockwise.  Defaults to 0.
+ * @param {Number} options.maxWidth Maximum barcode width (including quiet zones).  If specified, then the moduleWidth will be adjusted if necessary to make the entire barcode fit in the given width.
+ * @param {Number} options.width If given, then ignore moduleWidth and maxWidth and set the moduleWidth so that the barcode will have the given width.
+ */
+drawBarcode = bardcode.drawBarcode = function(g, text, options) {
+    // Validate input.
+    if (typeof g !== "object" && g !== "svg") {
+        throw new Error("drawBarcode: expected `g' to be an object or 'svg'.");
+    }
+
+    if (!text || typeof text !== "string") {
+        throw new Error("drawBarcode: expected `text' to be a non-empty string.");
+    }
+
+    if (typeof options !== "object") {
+        options = { };
+    }
+
+    copyDefaults(options, optionDefaults);
+    bardcode.validateDrawBarcodeOptions(options);
+
+    var encodeData;
+    switch (options.type) {
+        case "Codabar":
+            encodeData = bardcode.encodeCodabar(text);
+            break;
+        case "Code 128":
+            encodeData = bardcode.encodeCode128(text);
+            break;
+        case "Code 39":
+            encodeData = bardcode.encodeCode39(text);
+            break;
+        case "ITF":
+            encodeData = bardcode.encodeITF(text);
+            break;
+        case "EAN-8":
+            encodeData = bardcode.encodeEAN(text);
+            break;
+        case "EAN-13":
+            encodeData = bardcode.encodeEAN(text);
+            break;
+        case "FIM":
+            encodeData = bardcode.encodeFIM(text);
+            break;
+        case "UPC-A":
+            encodeData = bardcode.encodeEAN(text);
+            break;
+    }
+
+    switch (encodeData.type) {
+        case "bits":
+            return bardcode.drawBitsBarcode(g, options, encodeData);
+        default:
+            throw new Error("Unrecognized encoded barcode type: " + encodeData.type);
+    }
+};
+
+bardcode.validateDrawBarcodeOptions = function validateDrawBarcodeOptions(options) {
+    bardcode.AssertIsNumber(options.x, "options.x");
+    bardcode.AssertIsNumber(options.y, "options.y");
+    bardcode.AssertIsValidHorizontalAlign(options.horizontalAlign);
+    bardcode.AssertIsValidVerticalAlign(options.verticalAlign);
+    bardcode.AssertIsPositiveNumber(options.height, "options.height");
+    bardcode.AssertIsPositiveNumber(options.moduleWidth, "options.moduleWidth");
+    bardcode.AssertIsNonNegativeNumber(options.quietZoneSize, "options.quietZoneSize");
+    bardcode.AssertIsNumber(options.angle, "options.angle");
+    bardcode.AssertIsPositiveNumber(options.maxWidth, "options.maxWidth");
+
+    // width can either be NaN or a positive number.
+    if (!isNaN(options.width)) {
+        bardcode.AssertIsPositiveNumber(options.width, "options.width");
+    }
+};
+
+bardcode.drawBitsBarcode = function(g, options, encodeData) {
+    if (g === "svg") {
+        return bardcode.drawBitsBarcodeToSVG(options, encodeData);
+    } else {
+        return bardcode.drawBitsBarcodeToCanvas(g, options, encodeData);
+    }
+};
+
+bardcode.drawBitsBarcodeToSVG = function(options, encodeData) {
+    var bits = encodeData.data.map(function(d) {
+        return d.bits;
+    }).join("");
+
+    var fixedWidth;
+    var bw;
+    var width;
+
+    var multiplier = (bits.length + 2*options.quietZoneSize);
+
+    if (!isNaN(options.width)) {
+        // options.width takes precedence... if given, then it overrides
+        // moduleWidth and maxWidth
+        fixedWidth = true;
+        width = options.width;
+        bw = width/multiplier;
+    } else {
+        // Try to use the given moduleWidth
+        fixedWidth = false;
+        bw = options.moduleWidth;
+        width = multiplier*bw;
+
+        // But adjust if it doesn't fit in maxWidth (if given)
+        if (width > options.maxWidth) {
+            width = options.maxWidth;
+            bw = width/multiplier;
+        }
+    }
+
+    var height = options.height;
+
+    var svgLines = [];
+    svgLines.push("<svg " + [
+        "xmlns='http://www.w3.org/2000/svg'",
+        "width='" + width.toFixed(3) + "'",
+        "height='" + height + "'",
+        "fill='black'"
+    ].join(" ") + ">");
+
+    // Walk xpos from left to right side.
+    var xpos = options.quietZoneSize*bw;
+
+    var n = 0;
+    while (n < bits.length) {
+        // We are at the start of a bar or a space.
+        var bit = bits[n];
+        if (bit === "1") {
+            // We are at a bar.
+            var w = 1;
+            while (n < bits.length && bits[++n] === "1") {
+                w++;
+            }
+
+            var barWidth = w*bw;
+            svgLines.push("<rect " + [
+                "width='" + barWidth + "'",
+                "height='" + height + "'",
+                "x='" + xpos.toFixed(3) + "'",
+                "y='0'"
+            ].join(" ") + " />");
+            xpos += barWidth;
+        } else {
+            // We are at a space.
+            var w = 1;
+            while (n < bits.length && bits[++n] === "0") {
+                w++;
+            }
+
+            var spaceWidth = w*bw;
+            xpos += spaceWidth;
+        }
+    }
+
+    svgLines.push("</svg>");
+    return svgLines.join("\n");
+};
+
 
 bardcode.AssertIsString = function(x, name) {
     if (typeof x !== "string") {
